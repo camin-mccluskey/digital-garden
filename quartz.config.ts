da8712fea94191bfbd52c61f3f9c2d4c4b7c6088
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2023-09-02 17:29:16 +0100

    Push init notes/layout

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌱 Camin's Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.camin.xyz",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
